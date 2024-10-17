5b5928661bfaf9b12bd7fb03e766672e13f4b8e8
Matteo Freddi <matteo.freddi@hotmail.it>
2024-10-17 21:30:46 +0200

    example project to move nugetpackages output dir

---
+++ NuGetCustomFolder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGetCustomFolder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var d = Newtonsoft.Json.JsonConvert.SerializeObject(new { Name = "John Doe" });
+
+            //https://stackoverflow.com/questions/55946010/how-to-specify-output-folder-for-the-referenced-nuget-packages
+
+        }
+    }
+}
