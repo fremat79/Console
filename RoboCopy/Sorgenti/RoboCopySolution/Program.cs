5a825032013522a0ef8452d283e5c23fe43b266d
Matteo Freddi <matteo.freddi@hotmail.it>
2024-10-18 21:39:15 +0200

    add robocopy post buildevent example

---
+++ RoboCopy/Sorgenti/RoboCopySolution/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboCopySolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // double check post buld event, ignoring robocopy result code
+            // and retur 0 to prevent build error
+            // robocpy always return 1 if no files are copied
+
+            // https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/robocopy
+        }
+    }
+}
